820f9899899db8393fbb0cd31ab804355517cb9f
江斌 <jiangbin@botech.com.cn>
2022-03-13 10:54:56 +0800

    [代码重构](master): reformat
    reformat

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 说明
+
 1. 动态URL的springcloud-feign的熔断功能无效
 2. 需要自定义设置
 3. demo中做了简单说明
--- test/src/main/java/demo/InvocationRuntimeSetterFactory.java
+++ test/src/main/java/demo/InvocationRuntimeSetterFactory.java
@@ -17,45 +17,46 @@ import java.net.URI;
  */
 public interface InvocationRuntimeSetterFactory {
 
-  /**
-   * 创建 hystrix Setter
-   * @param target
-   * @param proxy
-   * @param method
-   * @param args
-   * @return
-   * @thows
-   * @author ying.chen2
-   * @date 2020/1/13
-   */
-  HystrixCommand.Setter create(final Target<?> target, final Object proxy, final Method method, final Object[] args);
+    /**
+     * 创建 hystrix Setter
+     *
+     * @param target
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @thows
+     * @author ying.chen2
+     * @date 2020/1/13
+     */
+    HystrixCommand.Setter create(final Target<?> target, final Object proxy, final Method method, final Object[] args);
 
 
-  final class Default implements InvocationRuntimeSetterFactory {
+    final class Default implements InvocationRuntimeSetterFactory {
 
-    @Override
-    public HystrixCommand.Setter create(final Target<?> target, final Object proxy, final Method method, final Object[] args) {
-      String groupKey = target.name();
-      String commandKey = Feign.configKey(target.type(), method);
+        @Override
+        public HystrixCommand.Setter create(final Target<?> target, final Object proxy, final Method method, final Object[] args) {
+            String groupKey = target.name();
+            String commandKey = Feign.configKey(target.type(), method);
 
-      Object anyUri = null;
-      if(args != null && args.length > 0) {
-        for (Object arg : args) {
-          if(arg instanceof URI) {
-            anyUri = arg;
-          }
+            Object anyUri = null;
+            if (args != null && args.length > 0) {
+                for (Object arg : args) {
+                    if (arg instanceof URI) {
+                        anyUri = arg;
+                    }
+                }
+            }
+
+            if (anyUri != null) {
+                URI uri = (URI) anyUri;
+                groupKey = groupKey + "#" + uri.toString();
+                commandKey = commandKey + "#" + uri.toString();
+            }
+
+            return HystrixCommand.Setter
+                    .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
+                    .andCommandKey(HystrixCommandKey.Factory.asKey(commandKey));
         }
-      }
-
-      if (anyUri != null) {
-        URI uri = (URI) anyUri;
-        groupKey = groupKey + "#" + uri.toString();
-        commandKey = commandKey + "#" + uri.toString();
-      }
-
-      return HystrixCommand.Setter
-              .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
-              .andCommandKey(HystrixCommandKey.Factory.asKey(commandKey));
     }
-  }
 }
